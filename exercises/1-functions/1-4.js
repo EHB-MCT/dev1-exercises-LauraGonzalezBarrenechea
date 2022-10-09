4571f8b98b5acf6c0371ecd716eb3084fe51089a
Laura Gonzalez <laura.gonzalez.barrenechea@student.ehb.be>
2022-10-09 15:38:26 +0200

    1-4 Created layers

---
+++ exercises/1-functions/1-4.js
@@ -0,0 +1,58 @@
+"use strict";
+
+drawLine();
+
+function drawLine() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    
+    context.fillStyle = "lightBlue";
+    context.lineWidth = "2";
+    context.beginPath();
+    context.rect(50, 50, 350, 350);
+    context.fill();
+    context.stroke();
+
+
+
+    context.fillStyle = "lightBlue";
+    context.lineWidth = "2";
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fill();
+    context.stroke();
+
+
+
+    context.fillStyle = "lightBlue";
+    context.lineWidth = "2";
+    context.beginPath();
+    context.rect(100, 50, 250, 250);
+    context.fill();
+    context.stroke();
+
+
+
+    context.fillStyle = "lightBlue";
+    context.lineWidth = "2";
+    context.beginPath();
+    context.rect(150, 100, 200, 200);
+    context.fill();
+    context.stroke();
+
+
+
+    context.fillStyle = "lightBlue";
+    context.lineWidth = "2";
+    context.beginPath();
+    context.rect(150, 150, 150, 150);
+    context.fill();
+    context.stroke();
+}
+
+
+ 
