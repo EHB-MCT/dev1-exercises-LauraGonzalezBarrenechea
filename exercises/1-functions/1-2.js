97f0d1fc96d9c44fce00aa038dbbbfec76911778
Laura Gonzalez <laura.gonzalez.barrenechea@student.ehb.be>
2022-10-09 10:09:21 +0200

    1-2 Created a box with black and red lines

---
+++ exercises/1-functions/1-2.js
@@ -0,0 +1,42 @@
+"use strict";
+
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   context.lineWidth = 3;
+   context.strokeStyle = 'black';
+   context.beginPath();
+   context.moveTo(50, 50);
+   context.lineTo(150, 50);
+   context.stroke();
+
+   context.beginPath();
+   context.moveTo(150, 50);
+   context.lineTo(150, 150);
+   context.lineTo(50, 150);
+   context.lineTo(50, 50);
+   context.stroke();
+
+
+   context.lineWidth = 3;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(50, 50);
+   context.lineTo(150, 150);
+   context.stroke();
+
+   context.lineWidth = 3;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(150, 50);
+   context.lineTo(50, 150);
+   context.stroke();
+
+}
+
